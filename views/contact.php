b266fabbf7952f61522c68a30717ceb326635c75
jpcodeplus <behrens.codeplus@gmail.com>
2024-02-28 23:56:13 +0100

    feat: Integrate Tailwind CSS and implement UI elements such as header, footer, and form

--- views/contact.php
+++ views/contact.php
@@ -1,5 +1,21 @@
-<h1>Contact us!</h1>
-<form method="post" action="/contact">
-<input name="demo"/>
-<button type="submit">senden</button>
+<h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-700 md:text-5xl lg:text-6xl text-center">Contact</h1>
+<h2 class="text-4xl font-extrabold text-center text-gray-600">Payments tool for companies</h2>
+
+
+<form class="max-w-lg mt-16 mx-auto">
+  <div class="mb-5">
+    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
+    <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@flowbite.com" required />
+  </div>
+  <div class="mb-5">
+    <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Your password</label>
+    <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@flowbite.com" required />
+  </div>
+  <div class="flex items-start mb-5">
+    <div class="flex items-center h-5">
+      <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300" required />
+    </div>
+    <label for="remember" class="ms-2 text-sm font-medium text-gray-900">Remember me</label>
+  </div>
+  <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
 </form>
